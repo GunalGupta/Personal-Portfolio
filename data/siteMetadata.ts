c2298b9d630512ef22d13d8661a9f08ec68e38c1
Gunal Gupta <97979413+GunalGupta@users.noreply.github.com>
2024-07-29 23:11:08 +0530

    Create siteMetadata.ts

---
+++ data/siteMetadata.ts
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+    title: "Gunal Gupta",
+    author: "Adarsha Acharya",
+    siteUrl: "https://gunalgupta.tech",
+    description:
+      "Gunal Gupta - Fullstack developer passionate about creating and sharing things on internet",
+    keywords: [
+      `Gunal Gupta, Gunal, Gupta, gunalgupta, Gunal Gupta Portfolio, gunalgupta.tech, gunal, gupta, gunal portfolio, gunal developer`,
+    ],
+    social: {
+      email: "guptagunal@gmail.com",
+      x: "https://x.com/GunalGupta",
+      linkedinLink: `https://www.linkedin.com/in/gunal-gupta/`,
+      githubLink: `https://github.com/GunalGupta`,
+    },
+  };
+  
