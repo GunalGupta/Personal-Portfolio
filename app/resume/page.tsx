4584999c8128fe3549720dc5bbe4b6cfee39a185
Gunal Gupta <97979413+GunalGupta@users.noreply.github.com>
2024-07-31 16:42:54 +0530

    Upgraded Resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,40 +1,14 @@
-import React from 'react';
-import Link from 'next/link';
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Download } from 'lucide-react';
+import type { Metadata } from "next";
+import Resume from "@/components/minimal-theme/Resume";
+
+export const metadata: Metadata = {
+  title: "Resume | Gunal Gupta",
+  description: "Hi, I'm Gunal Gupta you can view and download my resume from this page.",
+};
 
 const ResumePage: React.FC = () => {
   return (
-    <Card className="w-full max-w-4xl mx-auto my-4">
-      <CardContent className="p-6">
-        {/* PDF preview for larger screens */}
-        <div className="hidden sm:block">
-          <iframe
-            src="./_static/Gunal_Gupta_IIIT_Vadodara_ICD_Resume.pdf"
-            className="w-full h-[calc(100vh-200px)]"
-            title="Resume PDF"
-          />
-          <Button asChild className='mt-4 hover:scale-105 transform transition duration-200'>
-            <Link href="./_static/Gunal_Gupta_IIIT_Vadodara_ICD_Resume.pdf" download="Gunal_Gupta_IIIT_Vadodara_ICD_Resume.pdf" target='_blank'>
-              <Download className="mr-2 h-4 w-4" /> Download Resume
-            </Link>
-          </Button>
-        </div>
-
-        {/* Download button for mobile devices */}
-        <div className="sm:hidden flex flex-col items-center justify-center space-y-4">
-          <p className="text-center text-lg font-medium">
-            For better viewing experience on mobile, please download the resume.
-          </p>
-          <Button asChild>
-            <Link href="./_static/Gunal_Gupta_IIIT_Vadodara_ICD_Resume.pdf" download="resume.pdf">
-              <Download className="mr-2 h-4 w-4" /> Download Resume
-            </Link>
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+    <Resume />
   );
 };
 
